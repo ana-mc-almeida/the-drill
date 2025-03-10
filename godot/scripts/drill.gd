3b76ace023491d7baba1272b8847bd4f5ad9efa6
Alexandre Duarte <93451864+Crashim03@users.noreply.github.com>
2025-03-10 15:32:27 +0000

    Fix: drill max range (#15)

--- godot/scripts/drill.gd
+++ godot/scripts/drill.gd
@@ -3,8 +3,14 @@ extends Node2D
 @export var _max_distance: float = 50
 var _total_puzzles: int
 
+
+func _ready():
+	var start_node: Node2D = get_node("Start")
+	var end_node: Node2D =	get_node("End")
+	_max_distance = abs(start_node.position.y - end_node.position.y)
+
 func set_total_puzzles(total_puzzles: int) -> void:
 	_total_puzzles = total_puzzles
 
 func drill():
-	position.y += _max_distance / (_total_puzzles - 1)
+	position.y += _max_distance / (_total_puzzles)
